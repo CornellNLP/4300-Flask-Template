29d0bdcfe4581b49df1a7226e07346a27c733d89
Josh w. <32022650+Jooosh01@users.noreply.github.com>
2026-03-11 21:37:10 -0400

    implemented charCounts, started sent-anal
    created alg for character counts
    created alg for reverse postings
    
    started a barebones sent-anal

--- src/language-processing/character-counts.py
+++ src/language-processing/character-counts.py
@@ -1,3 +1,47 @@
+import pandas as pd
+import spacy
+from collections import Counter
+
 #first function get counts of characters output to  text file
+nlp = spacy.load("en_core_web_sm")
+docs = pd.read_csv("PLACEHOLDER CSV")
+comments = docs["comment"].dropna().tolist()
+def charCount():
+    docs = docs
+    comments = comments
+    character_counts = Counter()
+
+    for doc in nlp.pipe(docs, batch_size = 1000):
+        for ent in doc.ents:
+            if ent.label == "PERSON":
+                character_counts[ent.text] += 1
+    return character_counts
+
+
 #create dictionary of touples (text, list of characters in text, time)
 #create reverse postings for sent anal output to text file
+
+def createReversePostings():
+    docs = docs
+    comments = comments
+    reverse_postings = {}
+
+    texts = docs["comment"].astype(str)
+    ids = docs["id"]
+
+    for doc, comment_id in zip(nlp.pipe(texts, batch_size = 1000), ids):
+        persons = set(ent.text for ent in doc.ents if ent.label_ == "PERSON")
+
+        for person in persons:
+            reverse_postings[person].append(comment_id)
+
+    return reverse_postings
+
+print(charCount)
+print(createReversePostings)
+
+
+    
+    
+
+
