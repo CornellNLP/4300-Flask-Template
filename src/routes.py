39d67a3d5c4462275c7bf27d65a3312ad6ed6bf4
Ollie <derekethompson@gmail.com>
2026-03-20 23:37:37 -0400

    attempts at showing rankings

--- src/routes.py
+++ src/routes.py
@@ -6,6 +6,7 @@ To enable AI chat, set USE_LLM = True below. See llm_routes.py for LLM specific 
 import json
 from flask import render_template, request
 from models import db, Episode, Review
+from language_processing import similarity_calc
 
 # ── AI toggle ──
 USE_LLM = False
@@ -14,22 +15,27 @@ USE_LLM = False
 
 
 def json_search(query):
-    if not query or not query.strip():
-        query = "Kardashian"
-    results = db.session.query(Episode, Review).join(
-        Review, Episode.id == Review.id
-    ).filter(
-        Episode.title.ilike(f'%{query}%')
-    ).all()
-    matches = []
-    for episode, review in results:
-        matches.append({
-            'title': episode.title,
-            'descr': episode.descr,
-            'imdb_rating': review.imdb_rating
-        })
-    return json.dumps(matches)
+    # if not query or not query.strip():
+    #     query = "Kardashian"
+    # results = db.session.query(Episode, Review).join(
+    #     Review, Episode.id == Review.id
+    # ).filter(
+    #     Episode.title.ilike(f'%{query}%')
+    # ).all()
+    # matches = []
+    # for episode, review in results:
+    #     matches.append({
+    #         'title': episode.title,
+    #         'descr': episode.descr,
+    #         'imdb_rating': review.imdb_rating
+    #     })
+    # return json.dumps(matches)
 
+    if not query or not query.strip():
+        query = "Luffy"
+    name = similarity_calc.match_name(query, similarity_calc.char_list)
+    matches = similarity_calc.retrieve_k_docs(name, similarity_calc.tfidf_matrix, 10, similarity_calc.vectorizer, similarity_calc.ids, similarity_calc.docs)
+    return json.dumps(matches)
 
 def register_routes(app):
     @app.route("/")
